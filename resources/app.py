50ac7c1c7888036b5e5f0b6e3e9ed488035eb27e
David Hope <djhope@btinternet.com>
2024-11-01 06:49:25 -0400

    update log download

--- resources/app.py
+++ resources/app.py
@@ -19,25 +19,20 @@ app = Flask(__name__)
 def init():
     #assistant.load()
     #context.load()
-    truncated_logs_script = 'download-s3/download-logs.sh'
     full_logs_script = 'download-s3/download-logs.sh'
 
     # Set execute permissions on the shell scripts
     print("Setting execute permissions on shell scripts...")
-    subprocess.run(['chmod', '+x', truncated_logs_script], check=True)
     subprocess.run(['chmod', '+x', full_logs_script], check=True)
 
-    print("Running download-truncated-logs.sh...")
-    subprocess.run(['sudo', truncated_logs_script, 'truncated'], check=True)
+    print("Running download-full-logs.sh...")
+    subprocess.run(['sudo', full_logs_script, 'full'], check=True)
 
     integrations.load() #nginx, mysql
     enroll_elastic_agent.install_elastic_agent()
     slo.load() 
     ml.load_integration_jobs()
     #update ingest pipeline
-    print("Running download-full-logs.sh...")
-    subprocess.run(['sudo', full_logs_script, 'full'], check=True)
-    
     kibana.load() #dashboards
     
 
